8cf941e29ac529c317f8af94ad4ee70d71811550
Oz Ramos <git@mathiom.com>
2013-09-15 21:53:22 -0400

    Login Form
        ✔ Email @done (13-09-15 21:43)
        ✔ Password @done (13-09-15 21:43)
        ✔ Login Button @done (13-09-15 21:52)
        ✔ Model Binding @done (13-09-15 21:52)
    
    Signed-off-by: Oz Ramos <git@mathiom.com>

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,6 @@
 <?php
 
-Route::controller('me', 'DashboardController');
+Route::controller('me', 'DashboardController', ['getIndex'=>'dashboard']);
 
-Route::get('/', function(){ return View::make('pages.home'); });
+Route::get('/', ['as'=>'home', function(){ return View::make('pages.home'); } ]);
+Route::post('/', ['before'=>'csrf', function(){ return Redirect::route('home')->withInput(Input::except('password')); } ]);
--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.page')
 
 @section('content')
-    
+    {{Form::open(['route' => 'home'])}}
+        <div>
+            {{Form::label('email', 'Email:')}}
+            {{Form::text('email')}}
+        </div>
+        <div>
+            {{Form::label('password', 'Password:')}}
+            {{Form::password('password')}}
+        </div>
+        <div>
+            {{Form::submit('Login', ['name'=>'login[]'])}}
+        </div>
+    {{Form::close()}}
 @stop
