244d04d35490fb9bca3be26727154afdf7b70a35
Oz Ramos <git@mathiom.com>
2013-09-15 21:24:55 -0400

    Basic routes
    Routes:
    /index
    /me (dashboard)
    
    Signed-off-by: Oz Ramos <git@mathiom.com>

---
+++ app/views/layouts/page.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">  
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Mathiom</title>
+    <link rel="shortcut icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
